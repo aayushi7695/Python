1a48e81e58146ecbc213b48c15898c84556df48e
Aayushi <aayushi7695@gmail.com>
2020-09-12 23:50:26 +0530

    value from users in array with search added

--- Value from users in array.py
+++ Value from users in array.py
@@ -9,3 +9,15 @@ for i in range(n):
     arr.append(y)
 
 print(arr)
+
+z = int(input("Enter number to be searched in array"))
+k = 0
+for e in arr:
+    if e == z:
+        print("Index number of searched no. in array is", k)
+        break
+    k + 1
+else:
+    print("Searched no not in array")
+
+# print(arr.index(z))
