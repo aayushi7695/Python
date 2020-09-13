366f80270923db0a9fdff7ff08fe772a5b0d0c68
Aayushi <aayushi7695@gmail.com>
2020-09-13 13:41:00 +0530

    Practice2

---
+++ Practice2.py
@@ -0,0 +1,16 @@
+import string
+
+x = input('Enter 1st name')
+y = input('Enter 2nd name')
+
+print(y + " " + x)
+
+values = input('Enter some coma separated values')
+list = values.split(",")
+tuple = tuple(list)
+print('List=', list)
+print('Tuple=', tuple)
+
+file = input("enter file with extension")
+f_ext = file.split(".")
+print('Extension is' + repr(f_ext[-1]))
